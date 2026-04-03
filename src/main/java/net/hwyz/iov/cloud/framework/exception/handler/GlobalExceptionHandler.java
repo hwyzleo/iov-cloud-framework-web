03424171847f4e0f91814dd35dbf2d76e83269a7
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-04-03 10:59:52 +0800

    重构: 重构全局异常处理器

--- src/main/java/net/hwyz/iov/cloud/framework/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/net/hwyz/iov/cloud/framework/exception/handler/GlobalExceptionHandler.java
@@ -1,84 +1,48 @@
 package net.hwyz.iov.cloud.framework.exception.handler;
 
-import feign.FeignException;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import net.hwyz.iov.cloud.framework.common.exception.BaseException;
-import net.hwyz.iov.cloud.framework.common.exception.InputParamInvalidException;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import net.hwyz.iov.cloud.framework.common.exception.BusinessException;
+import net.hwyz.iov.cloud.framework.common.exception.CommonErrorCode;
+import net.hwyz.iov.cloud.framework.common.web.ApiResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MissingRequestHeaderException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
- * 全局异常处理
- *
- * @author hwyz_leo
+ * 全局异常处理器。
+ * <p>
+ * 统一捕获业务异常、参数校验异常和未知异常，返回标准 {@link ApiResponse}。
  */
 @Slf4j
-@RefreshScope
-@ControllerAdvice
-@ConfigurationProperties(prefix = "error")
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private Map<Integer, String> message;
-
-    public void setMessage(Map<Integer, String> message) {
-        this.message = message;
+    @ExceptionHandler(BusinessException.class)
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<Void> handleBusinessException(BusinessException ex) {
+        log.warn("业务异常: code={}, message={}", ex.getErrorCode().getCode(), ex.getMessage());
+        return ApiResponse.fail(ex.getErrorCode(), ex.getMessage());
     }
 
-    @ResponseBody
-    @ExceptionHandler({BaseException.class})
-    public Object serviceException(HttpServletResponse response, Throwable ex) {
-        response.setStatus(HttpStatus.OK.value());
-        BaseException exception = (BaseException) ex;
-        if (message != null && message.containsKey(exception.getCode())) {
-            exception.setMessage(message.get(exception.getCode()));
-        }
-        // TODO 设置APP
-        return exception;
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResponse<Void> handleValidationException(MethodArgumentNotValidException ex) {
+        String detail = ex.getBindingResult().getFieldErrors().stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining("; "));
+        log.warn("参数校验失败: {}", detail);
+        return ApiResponse.fail(CommonErrorCode.INVALID_PARAM, detail);
     }
 
-    @ResponseBody
-    @ExceptionHandler({IllegalArgumentException.class})
-    public Object illegalArgumentException(HttpServletResponse response, Throwable ex) {
-        response.setStatus(HttpStatus.OK.value());
-        InputParamInvalidException exception = new InputParamInvalidException(ex.getMessage());
-        // TODO 设置APP
-        return exception;
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiResponse<Void> handleException(Exception ex) {
+        log.error("系统异常", ex);
+        return ApiResponse.fail(CommonErrorCode.INTERNAL_ERROR);
     }
-
-    @ResponseBody
-    @ExceptionHandler({MissingRequestHeaderException.class})
-    public Object missingRequestHeaderExceptionException(HttpServletResponse response, Throwable ex) {
-        response.setStatus(HttpStatus.OK.value());
-        InputParamInvalidException exception = new InputParamInvalidException(ex.getMessage());
-        // TODO 设置APP
-        return exception;
-    }
-
-    @ResponseBody
-    @ExceptionHandler({FeignException.class})
-    public Object feignExceptionException(HttpServletResponse response, Throwable ex) {
-        response.setStatus(HttpStatus.OK.value());
-        BaseException exception = new BaseException(ex.getMessage());
-        // TODO 设置APP
-        return exception;
-    }
-
-    @ResponseBody
-    @ExceptionHandler({Exception.class})
-    public Object exceptionException(HttpServletResponse response, Throwable ex) {
-        response.setStatus(HttpStatus.OK.value());
-        logger.error("未知异常[{}]", ex.getMessage(), ex);
-        BaseException exception = new BaseException("未知异常");
-        // TODO 设置APP
-        return exception;
-    }
-
 }
