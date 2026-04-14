d681a0aac4f33f4c5cfacc13f438d89408f183f6
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-04-14 14:41:29 +0800

    改进: 优化Spring工具类并更新自动配置

--- src/main/java/net/hwyz/iov/cloud/framework/web/util/SpringUtil.java
+++ src/main/java/net/hwyz/iov/cloud/framework/web/util/SpringUtil.java
@@ -3,8 +3,8 @@ package net.hwyz.iov.cloud.framework.web.util;
 import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,15 +13,15 @@ import org.springframework.stereotype.Component;
  * @author hwyz_leo
  */
 @Component
-public final class SpringUtil implements BeanFactoryPostProcessor {
+public final class SpringUtil implements ApplicationContextAware {
     /**
      * Spring应用上下文环境
      */
-    private static ConfigurableListableBeanFactory beanFactory;
+    private static ApplicationContext applicationContext;
 
     @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        SpringUtil.beanFactory = beanFactory;
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringUtil.applicationContext = applicationContext;
     }
 
     /**
@@ -33,7 +33,10 @@ public final class SpringUtil implements BeanFactoryPostProcessor {
      */
     @SuppressWarnings("unchecked")
     public static <T> T getBean(String name) throws BeansException {
-        return (T) beanFactory.getBean(name);
+        if (applicationContext == null) {
+            return null;
+        }
+        return (T) applicationContext.getBean(name);
     }
 
     /**
@@ -44,7 +47,10 @@ public final class SpringUtil implements BeanFactoryPostProcessor {
      * @throws BeansException
      */
     public static <T> T getBean(Class<T> clz) throws BeansException {
-        T result = (T) beanFactory.getBean(clz);
+        if (applicationContext == null) {
+            return null;
+        }
+        T result = applicationContext.getBean(clz);
         return result;
     }
 
@@ -55,38 +61,19 @@ public final class SpringUtil implements BeanFactoryPostProcessor {
      * @return boolean
      */
     public static boolean containsBean(String name) {
-        return beanFactory.containsBean(name);
+        return applicationContext != null && applicationContext.containsBean(name);
     }
 
-    /**
-     * 判断以给定名字注册的bean定义是一个singleton还是一个prototype。 如果与给定名字相应的bean定义没有被找到，将会抛出一个异常（NoSuchBeanDefinitionException）
-     *
-     * @param name
-     * @return boolean
-     * @throws NoSuchBeanDefinitionException
-     */
     public static boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
-        return beanFactory.isSingleton(name);
+        return applicationContext != null && applicationContext.isSingleton(name);
     }
 
-    /**
-     * @param name
-     * @return Class 注册对象的类型
-     * @throws NoSuchBeanDefinitionException
-     */
     public static Class<?> getType(String name) throws NoSuchBeanDefinitionException {
-        return beanFactory.getType(name);
+        return applicationContext != null ? applicationContext.getType(name) : null;
     }
 
-    /**
-     * 如果给定的bean名字在bean定义中有别名，则返回这些别名
-     *
-     * @param name
-     * @return
-     * @throws NoSuchBeanDefinitionException
-     */
     public static String[] getAliases(String name) throws NoSuchBeanDefinitionException {
-        return beanFactory.getAliases(name);
+        return applicationContext != null ? applicationContext.getAliases(name) : null;
     }
 
     /**
