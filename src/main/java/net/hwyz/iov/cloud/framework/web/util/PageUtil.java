8d664abc73cccecbd1cbc3d9ca01b1128e83dcb4
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-04-21 14:59:21 +0800

    功能: 添加分页列表类型转换功能

--- src/main/java/net/hwyz/iov/cloud/framework/web/util/PageUtil.java
+++ src/main/java/net/hwyz/iov/cloud/framework/web/util/PageUtil.java
@@ -1,9 +1,14 @@
 package net.hwyz.iov.cloud.framework.web.util;
 
+import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import net.hwyz.iov.cloud.framework.common.util.SqlUtil;
 import net.hwyz.iov.cloud.framework.web.page.PageDomain;
 import net.hwyz.iov.cloud.framework.web.page.TableSupport;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * 分页工具类
@@ -28,5 +33,30 @@ public class PageUtil extends PageHelper {
      */
     public static void clearPage() {
         PageHelper.clearPage();
+    }
+
+    /**
+     * 将包含分页信息的列表转换为另一种类型的分页列表。
+     *
+     * @param originalList 原始分页列表
+     * @param mapper       转换函数
+     * @param <S>          原类型
+     * @param <T>          目标类型
+     * @return 转换后且保留分页信息的列表
+     */
+    public static <S, T> List<T> convert(List<S> originalList, Function<S, T> mapper) {
+        List<T> targetList = originalList.stream()
+                .map(mapper)
+                .collect(Collectors.toList());
+
+        if (originalList instanceof Page) {
+            Page<S> page = (Page<S>) originalList;
+            Page<T> newPage = new Page<>(page.getPageNum(), page.getPageSize(), page.isCount());
+            newPage.setTotal(page.getTotal());
+            newPage.addAll(targetList);
+            return newPage;
+        }
+
+        return targetList;
     }
 }
